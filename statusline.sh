40ae85b8476a65cbd5588f4f5eb0cec34b49805e
Christian Koue Muf <ckm@napatech.com>
2026-06-12 10:23:49 +0200

    Prints effort level

--- statusline.sh
+++ statusline.sh
@@ -8,6 +8,7 @@ G='\033[32m'  C='\033[36m'  W='\033[97m'  B='\033[1m'  N='\033[0m'
 
 eval "$(echo "$input" | jq -r '
   @sh "MODEL=\(.model.display_name // "Unknown")",
+  @sh "EFFORT=\(.effort.level // "")",
   @sh "CTX_SIZE=\(.context_window.context_window_size // 200000)",
   @sh "INPUT_TOK=\(.context_window.current_usage.input_tokens // 0)",
   @sh "OUTPUT_TOK=\(.context_window.current_usage.output_tokens // 0)",
@@ -21,5 +22,8 @@ CTX_SIZE="${CTX_SIZE:-200000}"
 
 fmt_k() { if [ "$1" -ge 1000 ]; then echo "$((${1} / 1000))k"; else echo "$1"; fi; }
 
+MODEL_LABEL="${MODEL:-Unknown}"
+[ -n "$EFFORT" ] && MODEL_LABEL="$MODEL_LABEL ($EFFORT)"
+
 printf "${G}${B}%s${N} | ${W}%s / %s${N} | ${W}%s${N}\n" \
-    "${MODEL:-Unknown}" "$(fmt_k $TOTAL_USED)" "$(fmt_k $CTX_SIZE)" "${CWD:-$PWD}"
+    "$MODEL_LABEL" "$(fmt_k $TOTAL_USED)" "$(fmt_k $CTX_SIZE)" "${CWD:-$PWD}"
